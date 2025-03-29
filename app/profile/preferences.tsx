d9ff0576026cc82b1156288672b6bb672105dd75
Wojtek Sokołowski <witekpan123@gmail.com>
2025-03-29 01:01:08 +0100

    preferences page first setup

--- app/profile/preferences.tsx
+++ app/profile/preferences.tsx
@@ -1,10 +1,20 @@
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { Text } from '@/src/components/ui/text';
 import { Link, useRouter } from 'expo-router';
 import { TouchableOpacity, View } from 'react-native';
 import { useTypedTranslation } from '@/src/hooks/useTypedTranslations';
 import { ChevronLeft } from '@/src/lib/icons';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/src/components/ui/select';
+import { Separator } from '@/src/components/ui/separator';
 
 const NAMESPACE = 'profile/preferences';
 const TRANSLATIONS = {
@@ -19,6 +29,14 @@ const TRANSLATIONS = {
 export default function App() {
   const router = useRouter();
   const { t } = useTypedTranslation(NAMESPACE, TRANSLATIONS);
+
+  const insets = useSafeAreaInsets();
+  const contentInsets = {
+    top: insets.top,
+    bottom: insets.bottom,
+    left: 12,
+    right: 12,
+  };
 
   return (
     <SafeAreaView className="flex items-center justify-center">
@@ -36,8 +54,34 @@ export default function App() {
         </Text>
       </View>
 
-      <hr className="my-8 h-px border-0 bg-gray-200 dark:bg-gray-700" />
-
+      <Select defaultValue={{ value: 'apple', label: 'Apple' }}>
+        <SelectTrigger className="w-[250px]">
+          <SelectValue
+            className="native:text-lg text-sm text-foreground"
+            placeholder="Select a fruit"
+          />
+        </SelectTrigger>
+        <SelectContent insets={contentInsets} className="w-[250px]">
+          <SelectGroup>
+            <SelectLabel>Fruits</SelectLabel>
+            <SelectItem label="Apple" value="apple">
+              Apple
+            </SelectItem>
+            <SelectItem label="Banana" value="banana">
+              Banana
+            </SelectItem>
+            <SelectItem label="Blueberry" value="blueberry">
+              Blueberry
+            </SelectItem>
+            <SelectItem label="Grapes" value="grapes">
+              Grapes
+            </SelectItem>
+            <SelectItem label="Pineapple" value="pineapple">
+              Pineapple
+            </SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
       {/* <Link href="/tabs/profile">
         <Text>(DEBUG) Go to profile</Text>
       </Link> */}
