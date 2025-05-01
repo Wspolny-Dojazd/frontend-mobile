95205e7cd0728b7dde7f6f7899737c972ba726ce
Sara Stec <sara.stec1002@gmail.com>
2025-05-01 23:19:48 +0200

    Integrate messages API for groupchats

--- app/chat/[id].tsx
+++ app/chat/[id].tsx
@@ -4,12 +4,16 @@ import {
     FlatList,
     TextInput,
     TouchableOpacity,
-    Image
+    Image,
+    Alert
   } from 'react-native';
   import Monicon from '@monicon/native';
   import { useRouter, useLocalSearchParams } from 'expo-router';
   import { SafeAreaView } from 'react-native-safe-area-context';
   import { useTypedTranslation } from '@/src/hooks/useTypedTranslations';
+  import { useAuth } from '@/src/context/authContext';
+  import { $api } from '@/src/api/api';
+  import { useCallback, useState, memo, useEffect, useRef } from 'react';
   
   const NAMESPACE = 'app/tabs/friends/[id]';
   const TRANSLATIONS = {
@@ -35,9 +39,9 @@ import {
   
   type Message = {
     id: number;
-    text: string;
-    time: string;
-    senderId: number;
+    userId: string;
+    content: string;
+    createdAt: string;
   };
   
   type ChatParams = {
@@ -49,7 +53,7 @@ import {
   
   // Mock data
   const mockUser = {
-    id: 1,
+    id: '1',
     name: 'You',
     imageSource: {
       uri: 'https://fastly.picsum.photos/id/852/200/200.jpg?hmac=4UHLpiS9j3YDnvq-w-MqnP5-ymiyvMs6BNV5ukoTRrI',
@@ -88,32 +92,103 @@ import {
   ];
   
   const mockMessages: Message[] = [
-    { id: 1, text: 'Hi, the restaurant is quite busy now so the delivery may be late 15 mins. Please wait for me.', time: '9:41', senderId: 1 },
-    { id: 2, text: 'Sure! Thank you', time: '12:03', senderId: 2 },
-    { id: 3, text: 'Could you please ask the restaurant to give me cutlery? I just need these items.', time: '12:03', senderId: 3 },
-    { id: 4, text: 'Yes, let me tell the restaurant.', time: '12:04', senderId: 4 },
-    { id: 5, text: 'Hi, the restaurant is quite busy now so the delivery may be late 15 mins. Please wait for me.', time: '9:41', senderId: 1 },
-    { id: 6, text: 'Sure! Thank you', time: '12:03', senderId: 2 },
-    { id: 7, text: 'Could you please ask the restaurant to give me cutlery? I just need these items.', time: '12:03', senderId: 3 },
-    { id: 8, text: 'Yes, let me tell the restaurant.', time: '12:04', senderId: 4 },
-    { id: 9, text: 'Hi, the restaurant is quite busy now so the delivery may be late 15 mins. Please wait for me.', time: '9:41', senderId: 1 },
-    { id: 10, text: 'Sure! Thank you', time: '12:03', senderId: 2 },
-    { id: 11, text: 'Could you please ask the restaurant to give me cutlery? I just need these items.', time: '12:03', senderId: 3 },
-    { id: 12, text: 'Yes, let me tell the restaurant.', time: '12:04', senderId: 4 },
-    { id: 13, text: 'Hi, the restaurant is quite busy now so the delivery may be late 15 mins. Please wait for me.', time: '9:41', senderId: 1 },
-    { id: 14, text: 'Sure! Thank you', time: '12:03', senderId: 2 },
-    { id: 15, text: 'Could you please ask the restaurant to give me cutlery? I just need these items.', time: '12:03', senderId: 3 },
-    { id: 16, text: 'Yes, let me tell the restaurant.', time: '12:04', senderId: 4 },
+    { id: 1, content: 'Hi, the restaurant is quite busy now so the delivery may be late 15 mins. Please wait for me.', createdAt: '9:41', userId: '1' },
+    { id: 2, content: 'Sure! Thank you', createdAt: '12:03', userId: '2' },
+    { id: 3, content: 'Could you please ask the restaurant to give me cutlery? I just need these items.', createdAt: '12:03', userId: '3' },
+    { id: 4, content: 'Yes, let me tell the restaurant.', createdAt: '12:04', userId: '4' },
+    { id: 5, content: 'Hi, the restaurant is quite busy now so the delivery may be late 15 mins. Please wait for me.', createdAt: '9:41', userId: '1' },
+    { id: 6, content: 'Sure! Thank you', createdAt: '12:03', userId: '2' },
+    { id: 7, content: 'Could you please ask the restaurant to give me cutlery? I just need these items.', createdAt: '12:03', userId: '3' },
+    { id: 8, content: 'Yes, let me tell the restaurant.', createdAt: '12:04', userId: '4' },
+    { id: 9, content: 'Hi, the restaurant is quite busy now so the delivery may be late 15 mins. Please wait for me.', createdAt: '9:41', userId: '1' },
+    { id: 10, content: 'Sure! Thank you', createdAt: '12:03', userId: '2' },
+    { id: 11, content: 'Could you please ask the restaurant to give me cutlery? I just need these items.', createdAt: '12:03', userId: '3' },
+    { id: 12, content: 'Yes, let me tell the restaurant.', createdAt: '12:04', userId: '4' },
+    { id: 13, content: 'Hi, the restaurant is quite busy now so the delivery may be late 15 mins. Please wait for me.', createdAt: '9:41', userId: '1' },
+    { id: 14, content: 'Sure! Thank you', createdAt: '12:03', userId: '2' },
+    { id: 15, content: 'Could you please ask the restaurant to give me cutlery? I just need these items.', createdAt: '12:03', userId: '3' },
+    { id: 16, content: 'Yes, let me tell the restaurant.', createdAt: '12:04', userId: '4' },
   ];
   
   export default function ChatScreen() {
     const { t } = useTypedTranslation(NAMESPACE, TRANSLATIONS);
     const router = useRouter();
-    
     const params = useLocalSearchParams<ChatParams>();
-    const members = params.chatType === 'group' 
-    ? mockGroupMembers 
-    : [];
+    const [messageInput, setMessageInput] = useState('');
+
+    const { token } = useAuth();
+    const { data: members } = $api.useQuery('get', '/api/groups/{id}/members', {
+      headers: { Authorization: `Bearer ${token}` },
+      params: { path: { id: Number(params.id) } },
+    });
+
+    const { data: groupMessages } = $api.useQuery(
+      'get',
+      '/api/groups/{id}/messages',
+      {
+        headers: { Authorization: `Bearer ${token}` },
+        params: { path: { id: Number(params.id) } },
+      },
+      {
+        refetchInterval: 1000,
+      }
+    );
+
+    const messages = params.chatType === 'group' 
+      ? groupMessages 
+      : mockMessages;
+
+    const { data: me } = $api.useQuery('get', '/api/auth/me', {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    const queryGroup = $api.useQuery(
+      'get',
+      '/api/groups/{id}',
+      {
+        headers: { Authorization: `Bearer ${token}` },
+        params: { path: { id: Number(params.id) } },
+      },
+      {
+        refetchInterval: 1000,
+      }
+    );
+  
+    const queryMembers = $api.useQuery(
+      'get',
+      '/api/groups/{id}/members',
+      {
+        headers: { Authorization: `Bearer ${token}` },
+        params: { path: { id: Number(params.id) } },
+      },
+      {
+        refetchInterval: 1000,
+      }
+    );
+
+    const mutationSendMessage = $api.useMutation('post', '/api/groups/{id}/messages');
+  
+    const handleSendMessage = useCallback(() => {
+      if (!messageInput.trim()) return;
+
+      const newMessage = messageInput;
+      setMessageInput('');
+
+      mutationSendMessage.mutate(
+        {
+          headers: { Authorization: `Bearer ${token}` },
+          params: { path: { id: Number(params.id) } },
+          body: { content: newMessage }
+        },
+        {
+          // onSuccess(data, variables, context) {
+          // },
+          onError(error, variables, context) {
+            Alert.alert(error?.code ?? 'Some error occurred');
+          },
+        }
+      );
+    }, [params.id, mutationSendMessage, token]);
   
     // Header
     const renderHeader = () => (
@@ -126,7 +201,7 @@ import {
           {params.chatType === 'group' ? (
             <View className="flex-row items-center mr-2">
               {/* Avatar container */}
-              <View className="relative mr-4 -top-1">
+              {/* <View className="relative mr-4 -top-1">
                 <Image
                   source={{ uri: members[0]?.imageSource?.uri }}
                   className="w-10 h-10 rounded-full border-2 border-background"
@@ -141,7 +216,7 @@ import {
               </View>
               {members.length > 2 && (
                 <Text className="text-gray-500 text-sm">+{members.length - 2}</Text>
-              )}
+              )} */}
             </View>
           ) : (
             <Image
@@ -168,20 +243,20 @@ import {
     );
   
     const renderMessage = ({ item }: { item: Message }) => {
-      const isCurrentUser = item.senderId === mockUser.id;
+      const isCurrentUser = item.userId === me?.id;
       const sender = params.chatType === 'private'
-      ? (item.senderId !== mockUser.id ? mockFriend : null) // For private chats, use mockFriend
-      : mockGroupMembers.find(f => f.id === item.senderId); // For groups, find in members
+      ? (item.userId !== mockUser.id ? mockFriend : null) // For private chats, use mockFriend
+      : members?.find(f => f.id === item.userId); // For groups, find in members
   
       return (
         <View className={`w-full flex-row ${isCurrentUser ? 'justify-end' : 'justify-start'} mb-3`}>
           {/* Sender's avatar */}
           {!isCurrentUser && sender && (
             <View className="mr-2 self-start">
-              <Image
+              {/* <Image
                 source={sender.imageSource}
                 className="w-10 h-10 rounded-full"
-              />
+              /> */}
             </View>
           )}
   
@@ -190,7 +265,7 @@ import {
             {/* Sender name for group chats */}
             {params.chatType === 'group' && !isCurrentUser && (
               <Text className="text-xs text-gray-500 mb-1">
-                {sender?.name}
+                {/* {sender?.name} */}
               </Text>
             )}
             
@@ -203,13 +278,13 @@ import {
               }`
             }>
               <Text className={`text-base ${isCurrentUser ? 'text-white' : 'text-black dark:text-white'}`}>
-                {item.text}
+                {item.content}
               </Text>
             </View>
             
             {/* Time stamp */}
             <Text className={`text-xs text-gray-500 mt-1 ${isCurrentUser ? 'text-right' : 'text-left'}`}>
-              {item.time}
+              {item.createdAt}
             </Text>
           </View>
     
@@ -230,9 +305,9 @@ import {
       <SafeAreaView className="flex-1 bg-background">
         {renderHeader()}
   
-         {/* Chat */}
-         <FlatList
-          data={mockMessages.slice().reverse()}
+        {/* Chat */}
+        <FlatList
+          data={messages?.slice().reverse()}
           renderItem={renderMessage}
           keyExtractor={item => item.id.toString()}
           className="p-4 flex-1"
@@ -264,8 +339,10 @@ import {
             className="flex-1 bg-muted rounded-full px-4 py-2 mr-2 dark:text-white"
             placeholder={t('message')}
             placeholderTextColor="#888"
+            value={messageInput}
+            onChangeText={setMessageInput}
           />
-          <TouchableOpacity className="px-2">
+          <TouchableOpacity className="px-2" onPress={handleSendMessage} disabled={!messageInput.trim()}>
             <Monicon name="fa:send-o" size={26} color='#3d917c' />
           </TouchableOpacity>
         </View>
