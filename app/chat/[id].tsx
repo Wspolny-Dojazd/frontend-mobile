a5154e280caf86846df065f5e1103ab55f2eefd3
Sara Stec <sara.stec1002@gmail.com>
2025-05-02 22:12:44 +0200

    Add date change indicators in chat

--- app/chat/[id].tsx
+++ app/chat/[id].tsx
@@ -4,7 +4,6 @@ import {
     FlatList,
     TextInput,
     TouchableOpacity,
-    Image,
     Alert
   } from 'react-native';
   import Monicon from '@monicon/native';
@@ -14,6 +13,7 @@ import {
   import { useAuth } from '@/src/context/authContext';
   import { $api } from '@/src/api/api';
   import { useCallback, useState, useMemo } from 'react';
+  import i18n from '@/i18n';
   
   const NAMESPACE = 'app/tabs/friends/[id]';
   const TRANSLATIONS = {
@@ -21,11 +21,15 @@ import {
       transit: 'During transit',
       active: 'Active now',
       message: 'Type a message...',
+      groupchat: 'Group chat',
+      noMessages: 'No messages found',
     },
     pl: {
       transit: 'W trakcie przejazdu',
       active: 'Aktywny/a teraz',
       message: 'Napisz wiadomość...',
+      groupchat: 'Czat grupy',
+      noMessages: 'Brak wiadomości',
     },
   };
   
@@ -43,40 +47,41 @@ import {
     members?: string;
   };
   
-  const mockFriend = {
-    id: 2,
-    nickname: 'John Cooper'
-  };
-  
-  const mockMessages: Message[] = [
-    { id: 1, content: 'Hi, the restaurant is quite busy now so the delivery may be late 15 mins. Please wait for me.', createdAt: '2025-05-02T11:55:03.490Z', userId: '1' },
-    { id: 2, content: 'Sure! Thank you', createdAt: '2025-05-02T11:55:03.490Z', userId: '2' },
-    { id: 3, content: 'Could you please ask the restaurant to give me cutlery? I just need these items.', createdAt: '2025-05-02T11:55:03.490Z', userId: '3' },
-    { id: 4, content: 'Yes, let me tell the restaurant.', createdAt: '2025-05-02T11:55:03.490Z', userId: '4' },
-    { id: 5, content: 'Hi, the restaurant is quite busy now so the delivery may be late 15 mins. Please wait for me.', createdAt: '2025-05-02T11:55:03.490Z', userId: '1' },
-    { id: 6, content: 'Sure! Thank you', createdAt: '2025-05-02T11:55:03.490Z', userId: '2' },
-    { id: 7, content: 'Could you please ask the restaurant to give me cutlery? I just need these items.', createdAt: '2025-05-02T11:55:03.490Z', userId: '3' },
-    { id: 8, content: 'Yes, let me tell the restaurant.', createdAt: '2025-05-02T11:55:03.490Z', userId: '4' },
-    { id: 9, content: 'Hi, the restaurant is quite busy now so the delivery may be late 15 mins. Please wait for me.', createdAt: '2025-05-02T11:55:03.490Z', userId: '1' },
-    { id: 10, content: 'Sure! Thank you', createdAt: '2025-05-02T11:55:03.490Z', userId: '2' },
-    { id: 11, content: 'Could you please ask the restaurant to give me cutlery? I just need these items.', createdAt: '2025-05-02T11:55:03.490Z', userId: '3' },
-    { id: 12, content: 'Yes, let me tell the restaurant.', createdAt: '2025-05-02T11:55:03.490Z', userId: '4' },
-    { id: 13, content: 'Hi, the restaurant is quite busy now so the delivery may be late 15 mins. Please wait for me.', createdAt: '2025-05-02T11:55:03.490Z', userId: '1' },
-    { id: 14, content: 'Sure! Thank you', createdAt: '2025-05-02T11:55:03.490Z', userId: '2' },
-    { id: 15, content: 'Could you please ask the restaurant to give me cutlery? I just need these items.', createdAt: '2025-05-02T11:55:03.490Z', userId: '3' },
-    { id: 16, content: 'Yes, let me tell the restaurant.', createdAt: '2025-05-02T11:55:03.490Z', userId: '4' },
-  ];
-  
   export default function ChatScreen() {
     const { t } = useTypedTranslation(NAMESPACE, TRANSLATIONS);
     const router = useRouter();
     const params = useLocalSearchParams<ChatParams>();
+    const language = i18n.language === 'pl' ? 'pl-PL' : 'en-US'
     const [messageInput, setMessageInput] = useState('');
 
     const { token } = useAuth();
     const { data: me } = $api.useQuery('get', '/api/auth/me', {
       headers: { Authorization: `Bearer ${token}` },
     });
+    
+    const mockMessages: Message[] = [
+      { id: 1, content: 'Hi, the restaurant is quite busy now so the delivery may be late 15 mins. Please wait for me.', createdAt: '2025-05-02T11:55:03.490Z', userId: me ? me.id : '1' },
+      { id: 2, content: 'Sure! Thank you', createdAt: '2025-05-02T11:55:03.490Z', userId: '2' },
+      { id: 3, content: 'Could you please ask the restaurant to give me cutlery? I just need these items.', createdAt: '2025-05-02T11:55:03.490Z', userId: '3' },
+      { id: 4, content: 'Yes, let me tell the restaurant.', createdAt: '2025-05-02T11:55:03.490Z', userId: '4' },
+      { id: 5, content: 'Hi, the restaurant is quite busy now so the delivery may be late 15 mins. Please wait for me.', createdAt: '2025-05-02T11:55:03.490Z', userId: me ? me.id : '1' },
+      { id: 6, content: 'Sure! Thank you', createdAt: '2025-05-02T11:55:03.490Z', userId: '2' },
+      { id: 7, content: 'Could you please ask the restaurant to give me cutlery? I just need these items.', createdAt: '2025-05-02T11:55:03.490Z', userId: '3' },
+      { id: 8, content: 'Yes, let me tell the restaurant.', createdAt: '2025-05-02T11:55:03.490Z', userId: '4' },
+      { id: 9, content: 'Hi, the restaurant is quite busy now so the delivery may be late 15 mins. Please wait for me.', createdAt: '2025-05-02T11:55:03.490Z', userId: me ? me.id : '1' },
+      { id: 10, content: 'Sure! Thank you', createdAt: '2025-05-02T11:55:03.490Z', userId: '2' },
+      { id: 11, content: 'Could you please ask the restaurant to give me cutlery? I just need these items.', createdAt: '2025-05-02T11:55:03.490Z', userId: '3' },
+      { id: 12, content: 'Yes, let me tell the restaurant.', createdAt: '2025-05-02T11:55:03.490Z', userId: '4' },
+      { id: 13, content: 'Hi, the restaurant is quite busy now so the delivery may be late 15 mins. Please wait for me.', createdAt: '2025-05-02T11:55:03.490Z', userId: me ? me.id : '1' },
+      { id: 14, content: 'Sure! Thank you', createdAt: '2025-05-02T11:55:03.490Z', userId: '2' },
+      { id: 15, content: 'Could you please ask the restaurant to give me cutlery? I just need these items.', createdAt: '2025-05-02T11:55:03.490Z', userId: '3' },
+      { id: 16, content: 'Yes, let me tell the restaurant.', createdAt: '2025-05-02T11:55:03.490Z', userId: '4' },
+    ];
+
+    const mockFriend = {
+      id: 2,
+      nickname: params.nickname
+    };
 
     const { data: members = [] } = $api.useQuery('get', '/api/groups/{id}/members',
       {
@@ -110,32 +115,8 @@ import {
     );
 
     const messages = params.chatType === 'group' 
-      ? groupMessages 
+      ? groupMessages?.slice().reverse()
       : mockMessages;
-
-    const queryGroup = $api.useQuery(
-      'get',
-      '/api/groups/{id}',
-      {
-        headers: { Authorization: `Bearer ${token}` },
-        params: { path: { id: Number(params.id) } },
-      },
-      {
-        refetchInterval: 1000,
-      }
-    );
-  
-    const queryMembers = $api.useQuery(
-      'get',
-      '/api/groups/{id}/members',
-      {
-        headers: { Authorization: `Bearer ${token}` },
-        params: { path: { id: Number(params.id) } },
-      },
-      {
-        refetchInterval: 1000,
-      }
-    );
 
     const mutationSendMessage = $api.useMutation('post', '/api/groups/{id}/messages');
   
@@ -194,7 +175,7 @@ import {
               )}
             </View>
           ) : (
-            <View className="w-10 h-10 items-center justify-center rounded-full border border-gray-200 bg-gray-100 dark:border-gray-800 dark:bg-gray-900">
+            <View className="w-10 h-10 mr-3 items-center justify-center rounded-full border border-gray-200 bg-gray-100 dark:border-gray-800 dark:bg-gray-900">
               <Text className='text-lg font-extrabold text-foreground'>
                 { params.nickname?.slice(0, 2) }
               </Text>
@@ -202,7 +183,7 @@ import {
           )}
           <View>
             <Text className="text-xl font-semibold text-black dark:text-white">
-              {params.nickname || 'Chat'}
+              {params.nickname || t('groupchat')}
             </Text>
             <Text className="text-xs text-gray-500 dark:text-gray-400 mt-0.5">
               {params.chatType === 'group' 
@@ -211,74 +192,98 @@ import {
             </Text>
           </View>
         </View>
-  
-        <TouchableOpacity className="p-2">
-          <Monicon name="tabler:location-filled" size={28} color="#3d917c" />
-        </TouchableOpacity>
+        
+        { params.chatType === 'private' && (
+          <TouchableOpacity className="p-2">
+            <Monicon name="tabler:location-filled" size={28} color="#3d917c" />
+          </TouchableOpacity>
+        )}
       </View>
     );
   
-    const renderMessage = ({ item }: { item: Message }) => {
+    const renderMessage = ({ item, prevItem }: { item: Message, prevItem?: Message | null; }) => {
       const isCurrentUser = item.userId === me?.id;
       const sender = params.chatType === 'private'
         ? (item.userId !== me?.id ? mockFriend : null)
         : members?.find(member => member.id === item.userId);
 
       const date = new Date(item.createdAt);
-      const time = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+      const time = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false });
+
+      const messageDateStr = date.toISOString().split('T')[0];
+      const prevMessageDateStr = prevItem ? new Date(prevItem.createdAt).toISOString().split('T')[0] : null;
+
+      const showDateSeparator = !prevItem || messageDateStr !== prevMessageDateStr;
     
       return (
-        <View className={`w-full flex-row ${isCurrentUser ? 'justify-end' : 'justify-start'} mb-3`}>
-          {/* Sender's avatar */}
-          {!isCurrentUser && sender && (
-            <View className="mr-2 self-start">
-              <View className="w-10 h-10 items-center justify-center rounded-full border border-gray-200 bg-gray-100 dark:border-gray-800 dark:bg-gray-900">
-                <Text className='text-lg font-extrabold text-foreground'>
-                  {sender?.nickname?.slice(0, 2)}
-                </Text>
+        <>
+          <View className={`w-full flex-row ${isCurrentUser ? 'justify-end' : 'justify-start'} mb-3`}>
+            {/* Sender's avatar */}
+            {!isCurrentUser && sender && (
+              <View className="mr-2 self-start">
+                <View className="w-10 h-10 items-center justify-center rounded-full border border-gray-200 bg-gray-100 dark:border-gray-800 dark:bg-gray-900">
+                  <Text className='text-lg font-extrabold text-foreground'>
+                    {sender?.nickname?.slice(0, 2)}
+                  </Text>
+                </View>
               </View>
-            </View>
-          )}
-    
-          {/* Message container */}
-          <View className={`max-w-[70%] ${isCurrentUser ? 'items-end' : 'items-start'}`}>
-            {/* Sender name for group chats */}
-            {params.chatType === 'group' && !isCurrentUser && (
-              <Text className="text-xs text-gray-500 mb-1 dark:text-gray-400">
-                {sender?.nickname}
-              </Text>
             )}
-            
-            {/* Message bubble */}
-            <View className={
-              `rounded-lg px-3 py-2 ${
-                isCurrentUser 
-                  ? 'bg-primary rounded-tr-sm' 
-                  : 'bg-muted rounded-tl-sm'
-              }`
-            }>
-              <Text className={`text-base ${isCurrentUser ? 'text-white' : 'text-black dark:text-white'}`}>
-                {item.content}
-              </Text>
-            </View>
-            
-            {/* Time stamp */}
-            <Text className={`text-xs text-gray-500 mt-1 ${isCurrentUser ? 'text-right' : 'text-left'}`}>
-              { time }
-            </Text>
-          </View>
-    
-          {/* Current user avatar */}
-          {isCurrentUser && (
-            <View className="ml-2 self-start">
-              <View className="w-10 h-10 items-center justify-center rounded-full border border-gray-200 bg-gray-100 dark:border-gray-800 dark:bg-gray-900">
-                <Text className='text-lg font-extrabold text-foreground'>
-                  { me?.nickname?.slice(0, 2) }
+      
+            {/* Message container */}
+            <View className={`max-w-[70%] ${isCurrentUser ? 'items-end' : 'items-start'}`}>
+              {/* Sender name for group chats */}
+              {params.chatType === 'group' && !isCurrentUser && (
+                <Text className="text-xs text-gray-500 mb-1 dark:text-gray-400">
+                  {sender?.nickname}
+                </Text>
+              )}
+              
+              {/* Message bubble */}
+              <View className={
+                `rounded-lg px-3 py-2 ${
+                  isCurrentUser 
+                    ? 'bg-primary rounded-tr-sm' 
+                    : 'bg-muted rounded-tl-sm'
+                }`
+              }>
+                <Text className={`text-base ${isCurrentUser ? 'text-white' : 'text-black dark:text-white'}`}>
+                  {item.content}
                 </Text>
               </View>
+              
+              {/* Time stamp */}
+              <Text className={`text-xs text-gray-500 mt-1 ${isCurrentUser ? 'text-right' : 'text-left'}`}>
+                { time }
+              </Text>
+            </View>
+      
+            {/* Current user avatar */}
+            {isCurrentUser && (
+              <View className="ml-2 self-start">
+                <View className="w-10 h-10 items-center justify-center rounded-full border border-gray-200 bg-gray-100 dark:border-gray-800 dark:bg-gray-900">
+                  <Text className='text-lg font-extrabold text-foreground'>
+                    { me?.nickname?.slice(0, 2) }
+                  </Text>
+                </View>
+              </View>
+            )}
+          </View>
+
+          {showDateSeparator && (
+            <View className="my-4 flex-row items-center justify-center">
+              <View className="flex-1 h-px bg-gray-300 dark:bg-gray-600" />
+              <Text className="mx-4 text-sm text-gray-500 dark:text-gray-400">
+                {date.toLocaleDateString(language, {
+                  weekday: 'short',
+                  year: 'numeric',
+                  month: 'short',
+                  day: 'numeric',
+                })}
+              </Text>
+              <View className="flex-1 h-px bg-gray-300 dark:bg-gray-600" />
             </View>
           )}
-        </View>
+        </>
       );
     };
   
@@ -288,8 +293,11 @@ import {
   
         {/* Chat */}
         <FlatList
-          data={messages?.slice().reverse()}
-          renderItem={renderMessage}
+          data={messages}
+          renderItem={({ item, index }) => {
+            const prevItem = (messages ?? [])[index + 1];
+            return renderMessage({ item, prevItem });
+          }}
           keyExtractor={item => item.id.toString()}
           className="p-4 flex-1"
           inverted
@@ -301,7 +309,7 @@ import {
           }}
           ListEmptyComponent={
             <View className="flex-1 items-center justify-center">
-              <Text className="text-gray-500">No messages found</Text>
+              <Text className="text-gray-500">{t('noMessages')}</Text>
             </View>
           }
         />
